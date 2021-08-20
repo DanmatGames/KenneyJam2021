f2d00a58c57c3088d46de426f045f5caa9b5a436
Darsirious <89274777+Darsirious@users.noreply.github.com>
2021-08-20 23:41:17 +0200

    Fonts + Piratas Giratorios
    :100:

---
+++ Assets/Scripts/Minigames/Piratas/ShipRotation.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShipRotation : MonoBehaviour
+{
+
+    public GameObject anchor;
+    public float velocidad;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        velocidad = 70f;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.RotateAround(anchor.transform.localPosition, Vector3.back, Time.deltaTime * velocidad);
+        transform.localRotation = Quaternion.Euler(0, 0, 0);
+    }
+}
